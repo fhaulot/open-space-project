107250203ddb01c7cf1d55c864de1dc2975e2f21
fhaulot <fhaulot@gmail.com>
2025-06-06 14:04:28 +0200

    updated seat class

--- table.py
+++ table.py
@@ -2,19 +2,25 @@
 First, the Seat, to know if the seat is free and what is the name of his occupant if there's one
 Second, the Table, to know how many seats are available and allow to assign people to a table"""
 
+
 class Seat :
     #we define attributes and their types
     def __init__(self, free : bool, occupant : str) :
         self.free = free
         self.occupant = occupant
+    def __str__(self):
+        return "The seast is {self.free} and it is not free there's {self.occupant}in it".format(self=self)
     #we see if the seat is available and put somebody on it if free
-    def set_occupant(self, name) :
+    def set_occupant(self, name : str) :
         if self.free == True :
-            self.set_occupant += name
-    #we remove the people on the seat if somebody's present
+            self.occupant = name
+            return self.free == False
+    #forget about this one!
     def remove_occupant(self) :
         if self.free != True :
-            self.remove_occupant -= self.occupant
+            return self.free == True
+
+
 
 class Table :
     #we define the attributes and their types
